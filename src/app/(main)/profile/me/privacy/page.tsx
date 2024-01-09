772df9c6a73dd8acb1a5792ce9ab20bc0e9fbe2a
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2024-01-09 16:57:17 -0300

    Add friends dropdown to privacy settings

--- src/app/(main)/profile/me/privacy/page.tsx
+++ src/app/(main)/profile/me/privacy/page.tsx
@@ -13,6 +13,15 @@ export default function Home() {
 					</select>
 				</div>
 
+				<div>
+					<label htmlFor="privacy-friends" className="block mb-2 text-sm font-medium text-gray-900">Visibilidad de tus amigos</label>
+					<select className="grid bg-gray-50 border w-fit border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 focus:ring-2 outline-none p-2.5" name="privacy-statistics" id="privacy-friends">
+						<option className="" value="all">Todas las personas</option>
+						<option className="" value="friends">Solo amigos</option>
+						<option className="" value="me">Solo yo</option>
+					</select>
+				</div>
+
 				<button className="btn-primary">Guardar cambios</button>
 			</form>
 		</div>
