63ad9d21eb051ba4b1c58d3b83075bb4a0d42adf
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2024-01-11 20:10:43 -0300

    Show ammount of likes and comments on food posts on main page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function NotFound() {
 	return (
-		<section className="bg-white">
+		<section className="bg-white min-h-screen">
 			<div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
 				<div className="mx-auto max-w-screen-sm text-center">
 					<h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600">404</h1>
---
+++ src/app/ui/main/post-food-comment.tsx
@@ -0,0 +1,15 @@
+import { getFoodCommentsCount } from "@/app/lib/data";
+import Link from "next/link";
+
+export default async function PostFoodComment({ postId }: { postId: string }) {
+	const commentsCount = await getFoodCommentsCount(postId);
+
+	return (
+		<Link href={`/posts/food/${postId}`} className="px-5 py-2.5 font-bold flex gap-2 items-center justify-center w-full hover:bg-gray-100 duration-100 hover:text-gray-900">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
+				<path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+			</svg>
+			Comentar {commentsCount.count > 0 ? `(${commentsCount.count})` : ''}
+		</Link>
+	)
+}
