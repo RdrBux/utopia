a0aee857b308aa2317d337618204debf0fed1b0e
rdrbux <rodriguezrodrigoemmanuel@gmail.com>
2024-01-15 21:07:45 -0300

    Fix missing attribute: name. In Input component

--- src/app/ui/input.tsx
+++ src/app/ui/input.tsx
@@ -10,7 +10,7 @@ export default function Input({ label, id, type = 'text', placeholder = '', ...p
 	return (
 		<div className="">
 			<label htmlFor={id} className="block mb-2 text-sm font-medium text-gray-900">{label}</label>
-			<input type={type} id={id} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 block w-full p-2.5 focus:ring-2 focus:border-primary-500 outline-none `} placeholder={placeholder} required {...props} />
+			<input type={type} id={id} name={id} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 block w-full p-2.5 focus:ring-2 focus:border-primary-500 outline-none `} placeholder={placeholder} required {...props} />
 		</div>
 	)
 }
